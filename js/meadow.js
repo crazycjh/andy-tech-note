4735b17afcc1957a13cb3e016354a22a860351e4
ChenHao Chen <crazycjh@gmail.com>
2024-04-21 01:58:03 +0800

    Site updated: 2024-04-21 01:58:03

--- js/meadow.js
+++ js/meadow.js
@@ -2,7 +2,7 @@
 if(typeof mdui.$ === "function"){
   var $$ = mdui.$;
 } else if(typeof mdui.$ === "undefined"){
-  var $$ = mduwei.JQ;
+  var $$ = mdui.JQ;
 } else {
   console.error("MDUI JS Not Found!");
 };
